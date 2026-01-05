f0b643cfa8c363f2174857b83864e62b49ecf059
JackWilson05 <jacksonrwilson05@gmail.com>
2026-01-05 13:35:18 -0500

    began p3, need to understand more about what casues palindromes to make efficient algo

---
+++ Problem3_LargestPalindome.py
@@ -0,0 +1,16 @@
+# find largest palindrome of 3 digit nums
+
+# 1 digit
+# 9x1 = 9
+
+# 2 digit 
+# 99x91
+# 11x11
+
+# 3 digit
+
+# naive solution -> iterate thru all combos starting at top
+
+
+
+
